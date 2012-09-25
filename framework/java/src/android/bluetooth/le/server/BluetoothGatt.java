fddc1059316c5fd9c7bf6d5e1fd11c475b853f21
Manuel Francisco Naranjo <naranjo.manuel@gmail.com>
2012-09-25 14:51:30 -0300

    tagging 0.5.4a

--- framework/java/src/android/bluetooth/le/server/BluetoothGatt.java
+++ framework/java/src/android/bluetooth/le/server/BluetoothGatt.java
@@ -54,7 +54,7 @@ public class BluetoothGatt extends IBluetoothGatt.Stub implements BlueZInterface
     public static final String BLUETOOTH_LE_SERVICE = BleConstants.BLUETOOTH_LE_SERVICE;
 
     public static int API_LEVEL = 5;
-    public static String FRAMEWORK_VERSION = "0.5.2";
+    public static String FRAMEWORK_VERSION = "0.5.4a";
 
     private Map<BluetoothGattID, AppWrapper> registeredApps = new HashMap<BluetoothGattID, AppWrapper>();
     private AppWrapper[] registeredAppsByID = new AppWrapper[Byte.MAX_VALUE];
