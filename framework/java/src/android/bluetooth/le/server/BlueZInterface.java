fda5026b5a49624e68b7792a81ffacdbf84f1310
Manuel Francisco Naranjo <naranjo.manuel@gmail.com>
2012-10-02 00:01:13 -0300

    changed license

--- framework/java/src/android/bluetooth/le/server/BlueZInterface.java
+++ framework/java/src/android/bluetooth/le/server/BlueZInterface.java
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2012 Naranjo Manuel Francisco
- * Copyright (c) 2011-2012, Code Aurora Forum. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
